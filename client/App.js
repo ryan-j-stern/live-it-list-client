37760ba3c3c83fd75d254c14b764c82b454a1bb1
Ryan Stern <ryanstern03@gmail.com>
2020-11-12 13:50:36 -0500

    cleaned up app.js by removing unnecessary code

--- client/App.js
+++ client/App.js
@@ -1,25 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 
 import StackNav from './src/components/navigation/stack.component';
-
-const HomeScreen = () => (
-  <View style={styles.app}>
-    <Text>Home Screen</Text>
-  </View>
-);
-
-const WelcomeScreen = ({navigation}) => (
-  <View style={styles.app}>
-    <Text>Hello World.</Text>
-    <Button
-      title="Go to home screen"
-      onPress={() => navigation.navigate('Home')}></Button>
-  </View>
-);
 
 const App = () => {
   return (
@@ -28,14 +12,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  app: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
-});
 
 export default App;
